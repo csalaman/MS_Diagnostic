921dbf00dcce4887b398ba6c2e448893a593cfeb
csalaman <csalaman@terpmail.umd.edu>
2017-01-31 22:26:52 -0500

    Created a tap button, counts the amounts of taps.

--- app/src/main/java/com/cmsc436/ms_diagnostic/TapActivity.java
+++ app/src/main/java/com/cmsc436/ms_diagnostic/TapActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /*
@@ -11,6 +12,8 @@ import android.widget.Toast;
 * */
 public class TapActivity extends AppCompatActivity {
 
+    int count = 0;
+    TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,15 +23,21 @@ public class TapActivity extends AppCompatActivity {
 
 
 
+        text = (TextView) findViewById(R.id.tap_count);
         final Button button = (Button) findViewById(R.id.button_tap);
         button.setOnClickListener(new View.OnClickListener() {
             int count_taps = 0;
+
             public void onClick(View v) {
                 // Perform action on click
-                count_taps++;
-                Toast.makeText(getApplicationContext(),count_taps,Toast.LENGTH_SHORT ).show();
+               count_taps++;
+               count = count_taps;
+               text.setText("You have tapped " + count + " times!");
             }
         });
+
+
+
 
 
     }
