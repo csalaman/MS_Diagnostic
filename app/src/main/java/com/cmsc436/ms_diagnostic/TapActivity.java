6b4fc5ed2ae5c027af6d997ac4232c9857d54107
Shubham Patel <patelrsam@gmail.com>
2017-01-31 21:18:20 -0500

    This is the initial push for the Tap Activity

---
+++ app/src/main/java/com/cmsc436/ms_diagnostic/TapActivity.java
@@ -0,0 +1,15 @@
+package com.cmsc436.ms_diagnostic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/*
+* This is the Activity for the Tapping Test
+* */
+public class TapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tap);
+    }
+}
