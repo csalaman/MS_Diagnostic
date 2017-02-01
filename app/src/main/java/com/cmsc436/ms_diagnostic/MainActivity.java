4eb8eaad5876b2efa2d0b9f4f0481a071cfb47c3
Shubham Patel <patelrsam@gmail.com>
2017-01-31 21:55:57 -0500

    Added the Tap button to go to the Tap Activity

--- app/src/main/java/com/cmsc436/ms_diagnostic/MainActivity.java
+++ app/src/main/java/com/cmsc436/ms_diagnostic/MainActivity.java
@@ -1,7 +1,9 @@
 package com.cmsc436.ms_diagnostic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // changes
+    public void toTapActivity(View view){
+        startActivity(new Intent(MainActivity.this,TapActivity.class));
+    }
+
 }
 // HElo
