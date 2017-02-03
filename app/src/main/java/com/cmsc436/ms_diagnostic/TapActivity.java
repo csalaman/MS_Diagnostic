b2ae8d256fe3b3d5769bc02cb0d688c43ded7e78
Shubham Patel <patelrsam@gmail.com>
2017-02-02 22:51:13 -0500

    Set timer accommodate both hands Started recording for both hands separately Sent an intent when done to send to Result activity

--- app/src/main/java/com/cmsc436/ms_diagnostic/TapActivity.java
+++ app/src/main/java/com/cmsc436/ms_diagnostic/TapActivity.java
@@ -1,6 +1,7 @@
 package com.cmsc436.ms_diagnostic;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.CountDownTimer;
 import android.os.Bundle;
 import android.view.View;
@@ -14,14 +15,18 @@ import android.widget.TextView;
 public class TapActivity extends Activity {
 
     int count = 0;
+    int rightHand = 0;
+    int leftHand = 0;
     //TextView text;
     TextView instructions;
+    TextView hand;
 
 
     CountDownTimer recordTimer;
     Boolean isDone = false;
     Button tap_button ;
     Button start_button;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +37,7 @@ public class TapActivity extends Activity {
         tap_button = (Button) findViewById(R.id.button_tap);
         start_button = (Button) findViewById(R.id.start_button);
         instructions = (TextView) findViewById(R.id.text_instructions);
+        hand = (TextView) findViewById(R.id.tap_hand_text);
 
         recordTimer = new CountDownTimer(10000,1000) {
 
@@ -45,9 +51,33 @@ public class TapActivity extends Activity {
                 instructions.setText("Done!" + " Count: " + count);
                 start_button.setVisibility(View.VISIBLE);
                 if(isDone){
+                    //left hand updated
+                    leftHand = count;
+                    // when both hands are done, the intent in initialized
+                    // both hand data is set in putExtra
+                    intent = new Intent(TapActivity.this, Results.class);
+                    intent.putExtra("LEFT",""+leftHand);
+                    intent.putExtra("RIGHT",""+rightHand);
+
+                    // The start button's text is changed to "Done"
+                    //new listener is added to send to the Result Activity
+                    start_button.setText(R.string.tap_activity_done);
+                    start_button.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            startActivity(intent);
+                        }
+                    });
 
                 }else{
+                    // now the next counter will go in the first if statment
                     isDone = true;
+                    // right hand recorded
+                    rightHand = count;
+                    //count reset for next hand
+                    count = 0;
+                    // Set hand Instruction to Right
+                    hand.setText("HAND: Right");
                 }
             }
 
