b396cb996573e7f747b9f978907193a16ec5359d
laurenkleach <lkleach@terpmail.umd.edu>
2017-02-10 18:01:41 -0500

    laurens edit

--- app/src/main/java/com/cmsc436/ms_diagnostic/DrawView.java
+++ app/src/main/java/com/cmsc436/ms_diagnostic/DrawView.java
@@ -1,15 +1,62 @@
 package com.cmsc436.ms_diagnostic;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.view.View;
 
 /**
- * Created By Lauren and Shubham P.
- * This Class implemernt a Drawable View to be used for the Tracing Activity
+ * Created By Lauren L. and Shubham P.
+ * This Class implement a Drawable View to be used for the Tracing Activity
  */
 
 public class DrawView extends View {
+    //drawing path
+    private Path drawPath;
+
+    //defines what to draw
+    private Paint canvasPaint ;
+
+    //defines how to draw
+    private Paint drawPaint;
+
+    //initial color
+    private int paintColor = 0xFF660000;
+
+    //canvas - holding pen, holds your drawings and transfers them to the view
+    private Canvas drawCanvas;
+
+    //canvas bitmap
+    private Bitmap canvasBitmap;
+
+    //brush size
+    private float currentBrushSize, lastBrushSize;
+
     public DrawView(Context context) {
         super(context);
+        drawPaint = new Paint();
+        drawPaint.setColor(paintColor);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(20);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        drawCanvas = new Canvas(canvasBitmap);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+        canvas.drawPath(drawPath, drawPaint);
     }
 }
