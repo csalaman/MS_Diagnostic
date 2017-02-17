0cd6553ddd7b6a1c165d04f7a790969d16903ac2
csalaman <csalaman@terpmail.umd.edu>
2017-02-17 18:34:56 -0500

    Added Balancer class, reads XYZ Coordinates

--- app/src/main/java/com/cmsc436/ms_diagnostic/Balancer.java
+++ app/src/main/java/com/cmsc436/ms_diagnostic/Balancer.java
@@ -1,16 +1,19 @@
 package com.cmsc436.ms_diagnostic;
 
-import android.content.ComponentName;
 import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class Balancer extends AppCompatActivity implements SensorEventListener {
@@ -21,52 +24,59 @@ public class Balancer extends AppCompatActivity implements SensorEventListener {
     TextView y_text;
     TextView z_text;
 
-    SensorService sensorService;
-    SensorService.LocalBinder binder;
+    public static int x;
+    public static int y;
+
+    CDrawView mCDrawView;
+    ShapeDrawable mDraw = new ShapeDrawable();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_balancer);
+
+        mCDrawView = new CDrawView(this);
+        setContentView(mCDrawView);
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+       // sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
 
         x_text = (TextView) findViewById(R.id.x_text);
         y_text = (TextView) findViewById(R.id.y_text);
         z_text = (TextView) findViewById(R.id.z_text);
 
-        Intent sensorIntent = new Intent(this,SensorService.class);
-
-        bindService(sensorIntent,connection,BIND_AUTO_CREATE);
 
 
 
     }
 
-    ServiceConnection connection = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-            binder = (SensorService.LocalBinder) service;
-            sensorService = binder.getServerInstance();
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-            sensorService = null;
-        }
-    };
-
-
-
     @Override
     public void onSensorChanged(SensorEvent event) {
-        x_text.setText("X: " + event.values[0]);
-        y_text.setText("Y: " + event.values[1]);
-        z_text.setText("Z: " + event.values[2]);
+//        x_text.setText("X: " + event.values[0]);
+//        y_text.setText("Y: " + event.values[1]);
+//        z_text.setText("Z: " + event.values[2]);
 
+        {
+            if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+                x = (int) Math.pow(event.values[0], 2);
+                y = (int) Math.pow(event.values[1], 2);
+            }
+        }
+    }
 
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+
+        // Register this class as a listener for the accelerometer sensor
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+        // ...and the orientation sensor
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+                SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
@@ -74,5 +84,43 @@ public class Balancer extends AppCompatActivity implements SensorEventListener {
 
     }
 
+    @Override
+    protected void onStop()
+    {
+        // Unregister the listener
+        sensorManager.unregisterListener(this);
+        super.onStop();
+    }
+
+    public class CDrawView extends View {
+
+        static final int width = 50;
+        static final int height = 50;
+
+        public CDrawView(Context context){
+            super(context);
+
+            mDraw = new ShapeDrawable(new OvalShape());
+            mDraw.getPaint().setColor(0xff74AC23);
+            mDraw.setBounds(x,y,x+width,y+height);
+        }
+
+        protected void onDraw(Canvas canvas){
+            RectF oval = new RectF(Balancer.x,Balancer.y, Balancer.x + width,Balancer.y + height);
+            Paint p = new Paint();
+            p.setColor(Color.BLUE);
+            canvas.drawColor(Color.BLACK);
+            canvas.drawOval(oval,p);
+
+            invalidate();
+        }
+
+    }
 
 }
+
+
+
+
+
+
